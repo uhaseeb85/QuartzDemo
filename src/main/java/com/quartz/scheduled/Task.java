6805a1c39290b19f1b284808dd1d7790eb9adcb5
uhaseeb85 <uhaseeb85@gmail.com>
2020-06-20 13:35:52 +0530

    Added spring boot scheduling.

---
+++ src/main/java/com/quartz/scheduled/Task.java
@@ -0,0 +1,20 @@
+package com.quartz.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Task {
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void cronJobSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job expression:: " + strDate);
+    }
+
+}
