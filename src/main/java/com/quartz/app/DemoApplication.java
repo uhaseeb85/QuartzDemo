66943073d1ae9a02808e1ff66c624b64adbd8837
uhaseeb85 <uhaseeb85@gmail.com>
2020-06-21 19:39:24 +0530

    Refactor

--- src/main/java/com/quartz/app/DemoApplication.java
+++ src/main/java/com/quartz/app/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan("com.quartz")
 @EnableJpaRepositories
 @EnableScheduling
 public class DemoApplication {
